738f1e3d50b4e4be096c0dcd780eb466395503b6
carol <carol072400@gmail.com>
2023-10-31 02:10:34 +0900

    feat(baseball): create skeleton code

---
+++ src/main/java/baseball/domain/Judgement.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+import java.util.*;
+
+public class Judgement {
+    public int correctCount(List<Integer> computer, List<Integer> player) {
+        return 0;
+    }
+
+    public boolean hasPlace(int placeNumber, int number) {
+        return false;
+    }
+
+}
---
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -0,0 +1,8 @@
+package baseball.domain;
+import java.util.*;
+
+public class NumberGenerator {
+    public List<Integer> createRandomNumbers() {
+        return null;
+    }
+}
