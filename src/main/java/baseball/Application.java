0a4df7532f52abe21e7a11961b759b3f184ad061
carol <carol072400@gmail.com>
2023-10-31 03:21:35 +0900

    feat(baseball): 기능 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 import java.util.*;
 
+import baseball.domain.NumberGenerator;
 import baseball.domain.Referee;
 
 
@@ -9,8 +10,27 @@ public class Application {
 
 
     public static void main(String[] args) {
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> computer = generator.createRandomNumbers();
         Referee referee = new Referee();
-        String result = referee.compare(Arrays.asList(7, 8, 9), Arrays.asList(1, 2, 3));
-        System.out.println(result);
+
+        //0볼 3스트라이크 될 때까지 -> while문 필요
+        String result = "";
+        while (!result.equals("0볼 3스트라이크")) {
+            result = referee.compare(computer, askNumbers());
+            System.out.println(result);
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static List<Integer> askNumbers() { //정적메서드
+        System.out.print("숫자를 입력해 주세요: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : input.split("")) {
+            numbers.add(Integer.valueOf(number));
+        }
+        return numbers;
     }
 }
